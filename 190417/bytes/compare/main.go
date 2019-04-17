04eff3e28c245f7d467d37c5861eabb21d7f3e29
Geunyoung Kim <kgysmart@gmail.com>
2019-04-17 12:18:55 +0200

    add bytes compare

---
+++ 190417/bytes/compare/main.go
@@ -0,0 +1,30 @@
+package main
+
+// https://golang.org/pkg/bytes/
+
+import "bytes"
+
+func main() {
+	// Interpret Compare's result by comparing it to zero.
+	var a, b []byte
+	if bytes.Compare(a, b) < 0 {
+		// a less b
+	}
+	if bytes.Compare(a, b) <= 0 {
+		// a less or equal b
+	}
+	if bytes.Compare(a, b) > 0 {
+		// a greater
+	}
+	if bytes.Compare(a, b) >= 0 {
+		// a greater or equal b
+	}
+
+	// Prefer Equal to Compare for equality comparisons.
+	if bytes.Equal(a, b) {
+		// a equal b
+	}
+	if !bytes.Equal(a, b) {
+		// a not equal b
+	}
+}
