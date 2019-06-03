5441140a413db93b2da48757aba28f3cbe76a734
Geunyoung Kim <kgysmart@gmail.com>
2019-06-03 08:44:21 +0200

    Changed path

---
+++ 2019/05-May/21-31/31/hands-on-software-architecture-with-golang/ch04/quick-sort/quick_sort.go
@@ -0,0 +1,30 @@
+package quicksort
+
+import "math/rand"
+
+func quickSort(array []int) []int {
+	if len(array) <= 1 {
+		return array
+	}
+
+	left, right := 0, len(array)-1
+
+	// Pick a povit randomly and move it to the end
+	pivot := rand.Int() % len(array)
+	array[pivot], array[right] = array[right], array[pivot]
+
+	// Partition
+	for i := range array {
+		if array[i] < array[right] {
+			array[i], array[left] = array[left], array[i]
+			left++
+		}
+	}
+
+	array[left], array[right] = array[right], array[left]
+
+	// Recurse
+	quickSort(array[:left])
+	quickSort(array[left+1:])
+	return array
+}
