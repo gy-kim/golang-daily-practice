32d1363c5400b54821b6c61fe4c54c9720b3df5e
Geunyoung Kim <kgysmart@gmail.com>
2019-07-11 09:10:21 +0200

    Add mastering go (ch03)

--- 2019/07-Jul/01-10/10/mastering-go/ch02/03_panic_recover/main.go
+++ 2019/07-Jul/01-10/10/mastering-go/ch02/03_panic_recover/main.go
@@ -5,9 +5,9 @@ import "fmt"
 func a() {
 	fmt.Println("Inside a()")
 	defer func() {
-		// if c := recover(); c != nil {
-		// 	fmt.Println("Recover inside a()!")
-		// }
+		if c := recover(); c != nil {
+			fmt.Println("Recover inside a()!")
+		}
 	}()
 	fmt.Println("About to call b()")
 	b()
--- 2019/07-Jul/01-10/10/mastering-go/ch03/02_copy_slice/main.go
+++ 2019/07-Jul/01-10/10/mastering-go/ch03/02_copy_slice/main.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
 	a6 := []int{-10, 1, 2, 3, 4, 5}
