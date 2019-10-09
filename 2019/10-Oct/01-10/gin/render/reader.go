57eb03a9038548a80df700955e98719e3b99f09a
Geunyoung Kim <kgysmart@gmail.com>
2019-10-09 10:08:22 +0200

    Add gin render

---
+++ 2019/10-Oct/01-10/gin/render/reader.go
@@ -0,0 +1,40 @@
+package render
+
+import (
+	"io"
+	"net/http"
+	"strconv"
+)
+
+// Reader contains the IO reader and its length, and custom ContentType and other headers.
+type Reader struct {
+	ContentType   string
+	ContentLength int64
+	Reader        io.Reader
+	Headers       map[string]string
+}
+
+// Render (Reader) writes data with custom ContentType and headers.
+func (r Reader) Render(w http.ResponseWriter) (err error) {
+	r.WriteContentType(w)
+	if r.ContentLength >= 0 {
+		r.Headers["Content-Length"] = strconv.FormatInt(r.ContentLength, 10)
+	}
+	r.writeHeaders(w, r.Headers)
+	_, err = io.Copy(w, r.Reader)
+	return
+}
+
+// WriteContentType (Reader) writes custom ContentType.
+func (r Reader) WriteContentType(w http.ResponseWriter) {
+	writeContentType(w, []string{r.ContentType})
+}
+
+func (r Reader) writeHeaders(w http.ResponseWriter, headers map[string]string) {
+	header := w.Header()
+	for k, v := range headers {
+		if header.Get(k) == "" {
+			header.Set(k, v)
+		}
+	}
+}
