a32b5198c562e9e7ae1596c9f71f2a4b58a01d7e
Geunyoung Kim <kgysmart@gmail.com>
2019-11-29 15:56:42 +0100

    Add gin

--- 2019/11-Nov/21-30/25-31/gin/binding/form.go
+++ 2019/11-Nov/21-30/25-31/gin/binding/form.go
@@ -26,3 +26,31 @@ func (formBinding) Bind(req *http.Request, obj interface{}) error {
 	}
 	return validate(obj)
 }
+
+func (formPostBinding) Name() string {
+	return "form-urlencoded"
+}
+
+func (formPostBinding) Bind(req *http.Request, obj interface{}) error {
+	if err := req.ParseForm(); err != nil {
+		return err
+	}
+	if err := mapForm(obj, req.PostForm); err != nil {
+		return err
+	}
+	return validate(obj)
+}
+
+func (formMultipartBinding) Name() string {
+	return "multipart/form-data"
+}
+
+func (formMultipartBinding) Bind(req *http.Request, obj interface{}) error {
+	if err := req.ParseMultipartForm(defaultMemory); err != nil {
+		return err
+	}
+	if err := mappingByPtr(obj, (*multipartRequest)(req), "form"); err != nil {
+		return err
+	}
+	return validate(obj)
+}
